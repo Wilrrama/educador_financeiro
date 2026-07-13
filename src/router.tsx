06ffefd8927578814a8f43b2999ae74e5107e89f
wilrrama <wilrrama@hotmail.com>
2026-07-13 15:29:00 -0300

    feat: button

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Button } from "./components/shared/Button";
+import { PiggyBank } from "lucide-react";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <h1>Formulário de Simulação</h1>,
-  },
-  {
-    path: "/resultado",
-    element: <h1>Resultado da Simulação</h1>,
-  },
-  {
-    path: "/historico",
-    element: <h1>Histórico de Simulações</h1>,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <h1>Formulario de Simulação</h1>
+            {/* <Button variant="primary" icon={PiggyBank}>
+              clique aqui
+            </Button> */}
+          </>
+        ),
+      },
+      {
+        path: "/resultado",
+        element: <h1>Resultado de Simulação</h1>,
+      },
+      {
+        path: "/historico",
+        element: <h1>Historico de Simulação</h1>,
+      },
+    ],
   },
 ]);
