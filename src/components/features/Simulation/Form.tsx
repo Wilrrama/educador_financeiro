de823b405bac751b30563afdc3dc3944d23057de
wilrrama <wilrrama@hotmail.com>
2026-08-02 19:27:10 -0300

    feat: Card e Input

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,21 @@
+import { PiggyBank } from "lucide-react";
 import { StepProgress } from "./Progress";
+import { FormStep } from "./FormStep";
 
 export const SimulationForm = () => {
   return (
     <>
-      <StepProgress currentStep={3} totalSteps={6} />
+      <StepProgress currentStep={3} totalSteps={10} />
+      <FormStep
+        icon={PiggyBank}
+        title="Renda mensal bruta"
+        question="Quanto é depositado na sua conta todo mês"
+        inputProps={{
+          type: "text",
+          placeholder: "ex: 5.000,00",
+          prefix: "R$",
+        }}
+      />
     </>
   );
 };
